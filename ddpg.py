e1b0e9c802cd097400c1c853c85966f3dca29212
jardeldyonisio <jardel.dyonisio@hotmail.com>
2025-02-11 08:32:13 -0300

    :construction: first commit

---
+++ ddpg.py
@@ -0,0 +1,94 @@
+import torch
+import numpy as np
+import torch.autograd
+import torch.nn as nn
+import torch.optim as optim
+
+from models import *
+from utils import Memory
+from ddpg import Actor, Critic
+
+'''
+This class implements a DDPG agent.
+'''
+
+class DDPGAgent:
+    def __init__(self, state_dim, action_dim, max_action, gamma=0.99, tau=0.005, buffer_size=100000, 
+                 actor_learning_rate=1e-4, critic_learning_rate=1e-3):
+        
+        # Parameters
+        self.gamma = gamma
+        self.tau = tau
+
+        # Networks
+        self.actor = Actor(state_dim, action_dim, max_action)
+        self.actor_target = Actor(state_dim, action_dim, max_action)
+        
+        self.critic = Critic(state_dim, action_dim)
+        self.critic_target = Critic(state_dim, action_dim)
+        
+        for target_param, param in zip(self.actor_target.parameters(), self.actor.parameters()):
+            target_param.data.copy_(param.data)
+        
+        for target_param, param in zip(self.critic_target.parameters(), self.critic.parameters()):
+            target_param.data.copy_(param.data)
+
+        # Training
+        self.memory = Memory(buffer_size)
+        self.critic_criterion = nn.MSELoss()
+        self.actor_optimizer = optim.Adam(self.actor.parameters(), lr = actor_learning_rate)
+        self.critic_optimizer = optim.Adam(self.critic.parameters(), lr = critic_learning_rate)
+    
+    def get_action(self, state):
+        '''
+        This function is responsible for getting the action from the actor network.
+        '''
+        state = torch.FloatTensor(state).unsqueeze(0)
+        print('state: ', state)
+        action = self.actor.forward(state)
+        # action = self.actor(state).cpu().detach().numpy().flatten()
+        action = action.detach().numpy()[0,0]
+        print("action: ", action)
+        return action
+    
+    def update(self, batch_size):
+
+        '''
+        This function is responsible for updating the networks.
+        '''
+
+        # Get a batch of experiences
+        states, actions, rewards, next_states, _ = self.memory.sample(batch_size)
+
+        # Convert the experiences to PyTorch tensors
+        # for training with the networks
+        states = torch.FloatTensor(states)
+        actions = torch.FloatTensor(actions)
+        rewards = torch.FloatTensor(rewards)
+        next_states = torch.FloatTensor(next_states)
+
+        # Critic loss
+        Qvals = self.critic.forward(states, actions)
+        next_actions = self.actor_target.forward(next_states)
+        next_Q = self.critic_target.forward(next_states, next_actions.detach())
+        Qprime = rewards + self.gamma * next_Q
+        critic_loss = self.critic_criterion(Qvals, Qprime)
+        
+        # Actor loss
+        policy_loss = -self.critic.forward(states, self.actor.forward(states)).mean()
+        
+        # Update networks
+        self.actor_optimizer.zero_grad()
+        policy_loss.backward()
+        self.actor_optimizer.step()
+        
+        self.critic_optimizer.zero_grad()
+        critic_loss.backward()
+        self.critic_optimizer.step()
+
+        # Update target networks
+        for target_param, param in zip(self.actor_target.parameters(), self.actor.parameters()):
+            target_param.data.copy_(param.data * self.tau + target_param.data * (1.0 - self.tau))
+        
+        for target_param, param in zip(self.critic_target.parameters(), self.critic.parameters()):
+            target_param.data.copy_(param.data * self.tau + target_param.data * (1.0 - self.tau))
---
+++ envs/backup.py
@@ -0,0 +1,125 @@
+import gymnasium as gym
+import numpy as np
+import matplotlib.pyplot as plt
+import random
+
+class MultiAgentPathFollowingEnv(gym.Env):
+    def __init__(self, num_agents=20, buffer_size=1000):
+        super(MultiAgentPathFollowingEnv, self).__init__()
+        self.num_agents = num_agents
+        self.buffer_size = buffer_size
+        self.replay_buffer = []  # Replay buffer para armazenar transições
+        
+        # Action space: [steering, speed] for each agent
+        self.action_space = gym.spaces.Box(
+            low=np.array([[-0.5, 0.1]] * num_agents),
+            high=np.array([[0.5, 1.0]] * num_agents),
+            dtype=np.float32
+        )
+        
+        # Observation space: [x, y, distance_to_path, angle_to_path] per agent
+        self.observation_space = gym.spaces.Box(
+            low=-np.inf,
+            high=np.inf,
+            shape=(num_agents, 4),
+            dtype=np.float32
+        )
+        
+        self.reset()
+
+    def reset(self):
+        self.positions = np.zeros((self.num_agents, 2))  # Posições iniciais
+        self.orientations = np.zeros(self.num_agents)  # Orientações iniciais
+        self.path = self.generate_path()
+        self.time = 0
+        self.active_agents = np.ones(self.num_agents, dtype=bool)  # Agentes ativos
+        return self.get_state(), {}
+
+    def get_state(self):
+        distances, angles = self.calculate_path_metrics()
+        return np.column_stack((self.positions, distances, angles))
+
+    def step(self, actions):
+        self.update_positions(actions)
+        distances, angles = self.calculate_path_metrics()
+        rewards = self.calculate_rewards(distances, angles)
+        
+        # Marcar agentes como inativos se atingirem a distância de 5.0
+        self.active_agents = self.active_agents & (distances <= 5.0)
+
+        # Armazenar a transição no replay buffer
+        next_state = self.get_state()
+        for i in range(self.num_agents):
+            self.replay_buffer.append((self.get_state(), actions[i], rewards[i], next_state[i]))
+        
+        # Garantir que o replay buffer não ultrapasse o tamanho máximo
+        if len(self.replay_buffer) > self.buffer_size:
+            self.replay_buffer.pop(0)
+
+        self.time += 1
+        done = self.time > 500 or np.sum(self.active_agents) == 0  # Acaba o episódio
+        return next_state, rewards, done, {}
+
+    def render(self, mode='human'):
+        if not hasattr(self, 'fig'):
+            self.fig, self.ax = plt.subplots()
+            self.path_plot, = self.ax.plot([], [], linestyle='--', label='Path')
+            self.agent_plots = [self.ax.plot([], [], 'o', label=f'Agent {i}')[0] for i in range(self.num_agents)]
+            self.ax.set_xlim(-10, 110)
+            self.ax.set_ylim(-10, 10)
+            self.ax.legend()
+            plt.ion()
+            plt.show(block=False)
+
+        path_x, path_y = zip(*self.path)
+        self.path_plot.set_data(path_x, path_y)
+
+        for i, agent_plot in enumerate(self.agent_plots):
+            if self.active_agents[i]:
+                agent_plot.set_data(self.positions[i, 0], self.positions[i, 1])
+
+        self.fig.canvas.draw_idle()
+        self.fig.canvas.flush_events()
+
+    def update_positions(self, actions):
+        steerings, speeds = actions[:, 0], actions[:, 1]
+        self.orientations += steerings * 0.1
+        dx = speeds * np.cos(self.orientations)
+        dy = speeds * np.sin(self.orientations)
+        self.positions += np.column_stack((dx, dy))
+
+    def calculate_path_metrics(self):
+        distances = np.linalg.norm(self.path[:, None, :] - self.positions[None, :, :], axis=2)
+        closest_indices = np.argmin(distances, axis=0)
+        closest_points = self.path[closest_indices]
+        
+        distance_to_path = distances[closest_indices, np.arange(self.num_agents)]
+        path_directions = np.arctan2(closest_points[:, 1] - self.positions[:, 1],
+                                     closest_points[:, 0] - self.positions[:, 0])
+        angle_to_path = path_directions - self.orientations
+        
+        return distance_to_path, angle_to_path
+
+    def calculate_rewards(self, distances, angles):
+        return -distances - 0.1 * (angles ** 2)
+
+    def generate_path(self):
+        return np.array([[i, 0.0] for i in range(100)])
+
+    def sample_from_replay_buffer(self, batch_size):
+        """Retorna uma amostra aleatória do replay buffer."""
+        return random.sample(self.replay_buffer, batch_size) if len(self.replay_buffer) >= batch_size else []
+
+# Teste com múltiplos agentes
+num_agents = 20
+env = MultiAgentPathFollowingEnv(num_agents)
+
+while True:
+    obs, _ = env.reset()
+    for _ in range(500):
+        actions = env.action_space.sample()
+        obs, rewards, done, _ = env.step(actions)
+        env.render()
+        if done:
+            print(f"Episode ended: Time={env.time}")
+            break
