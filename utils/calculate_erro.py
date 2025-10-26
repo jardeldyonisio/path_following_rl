9dcdff281c44d65ce4833ed22f083f8deed7c0b5
jardeldyonisio <jardel.dyonisio@hotmail.com>
2025-10-26 10:33:16 -0300

    :construction: some changes to generate results

---
+++ utils/calculate_erro.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+#coding: utf-8
+
+import numpy as np
+
+from read_coords_from_file import read_points_from_file
+
+def calculate_minimum_distance(curva1, curva2):
+    distancias = []
+    for point1 in curva1:
+        min_dist = np.min(np.sqrt(np.sum((point1 - curva2) ** 2, axis=1)))
+        distancias.append(min_dist)
+    return np.mean(distancias)
+
+def calculate_erro(path_file_1, path_file_2):
+    points_path1 = read_points_from_file(path_file_1)
+    points_path2 = read_points_from_file(path_file_2)
+
+    erro_medio = calculate_minimum_distance(points_path1, points_path2)
+    
+    valor_referencia = np.mean(np.sqrt(np.sum(points_path1 ** 2, axis=1)))
+    
+    # Erro percentual
+    erro_percentual = (erro_medio / valor_referencia) * 100
+    print("Erro percentual entre as curvas:", erro_percentual)
+    return erro_percentual
+
+if __name__ == "__main__":
+    path_file_1 = '/home/jardeldyonisio/Documents/path_following_rl/paths/path_eight.txt'
+    path_file_2 = '/home/jardeldyonisio/Documents/path_following_rl/trajectories/trajectory_path_eight_2025-09-29_20-59-39.txt'
+    calculate_erro(path_file_1, path_file_2)
